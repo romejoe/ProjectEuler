e3cd46f51b31939bfcd05638674b95b4eb5380ec
Joseph Stanton <jsromejoe@gmail.com>
2015-12-29 20:57:18 -0500

    Completed p102

--- src/main/scala/p102/Main.scala
+++ src/main/scala/p102/Main.scala
@@ -1,23 +1,18 @@
 package p102
+import math._
 
 object Main extends App{
-  case class Point(x:Double, y:Double){
-    def -(p:Point):Point = Point(x-p.x, y-p.y)
-    def *(p:Point):Double = x*p.x + y*p.y
-
-    def x(p:Point):Double = (x*p.y)-(y*p.x)
-  }
+  case class Point(x:Double, y:Double)
 
   case class Line(a:Point, b:Point){
 
-    def asPointSlope = Point(b.x - a.x, b.y - a.y)
+    val M = (b.y-a.y)/(b.x-a.x)
+    val B = a.y - a.x*M
 
-    def doesIntersect(l:Line):Boolean = {
-      val (c, d) = l
+    def isDefinedAt(x:Double):Boolean = x <= max(a.x, b.x) && x >= min(a.x,b.x)
 
-
-
-
+    val asFn = (x:Double) => {
+      M*x + B
     }
   }
 
@@ -25,11 +20,27 @@ object Main extends App{
     def apply(arr:Array[Double]):Triangle = Triangle(Point(arr(0), arr(1)),Point(arr(2), arr(3)),Point(arr(4), arr(5)))
   }
 
+  def isBetween(a:Double, b:Double, y:Double):Boolean = {
+    y <= max(a,b) && y >= min(a,b)
+  }
+
   case class Triangle(a:Point, b:Point, c:Point){
     def centroid:Point = Point((a.x + b.x + c.x)/3.0, (a.y + b.y + c.y)/3.0)
     def AB = Line(a,b)
     def AC = Line(a,c)
     def BC = Line(b,c)
+
+    def containsPoint(p:Point):Boolean = {
+      val (x,y) = (p.x,p.y)
+      Seq(AB,AC,BC)
+          .filter(_.isDefinedAt(x))
+        .combinations(2)
+            .map(_.map(_.asFn(x)))
+          .exists(seq =>{
+            isBetween(seq.head, seq(1),y)
+          })
+    }
+
   }
 
   val ORIGIN = Point(0.0, 0.0)
@@ -41,11 +52,5 @@ object Main extends App{
     .map(_.trim.split(",").map(_.toDouble))
     .map(Triangle(_))
 
-  println(triangles.count(t => {
-    val testLine = Line(ORIGIN, t.centroid)
-    !(testLine.doesIntersect(t.AB) ||
-      testLine.doesIntersect(t.AC) ||
-      testLine.doesIntersect(t.BC))
-  }))
-
+  println(triangles.count(_.containsPoint(ORIGIN)))
 }
