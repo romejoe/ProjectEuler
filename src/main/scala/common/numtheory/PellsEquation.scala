a167d356e4a04f29630bc744b12e0eb2d39597e5
Joseph Stanton <jsromejoe@gmail.com>
2016-01-11 12:19:16 -0500

    Optimized Pell equation solver

--- src/main/scala/common/numtheory/PellsEquation.scala
+++ src/main/scala/common/numtheory/PellsEquation.scala
@@ -3,68 +3,75 @@ package common.numtheory
 import scala.collection.mutable.ArrayBuffer
 
 object PellsEquation {
-  def solvePellsEquation(D:Long):(BigInt,BigInt) = {
-    implicit def BDtoD(bd:BigDecimal):Double = bd.toDouble
+  def solvePellsEquation(D: Long): (BigInt, BigInt) = {
 
-    val a_0 = math.floor(math.sqrt(D)).toLong
 
-    val P: ArrayBuffer[BigInt] = ArrayBuffer()
-    P += 0 // 0
-    P += a_0 // 1
+    val a_0 = BigInt(math.sqrt(D).toLong)
 
-    val Q: ArrayBuffer[BigInt] = ArrayBuffer()
-    Q += 1 // 0
-    Q += D - a_0*a_0 // 1
+    var P_n1: BigInt = 0
+    var P_n: BigInt = a_0
 
-    val a:ArrayBuffer[BigInt] = ArrayBuffer()
+    var Q_n1: BigInt = 1
+    var Q_n: BigInt = D - a_0 * a_0
+
+    val a: ArrayBuffer[BigInt] = ArrayBuffer()
     a += a_0 // 0
-    a +=  ((a_0 + P(1).toLong)/Q(1)) // 1
+    a += ((a_0 + P_n.toLong) / Q_n) // 1
 
-    val p: ArrayBuffer[BigInt] = ArrayBuffer()
-    p += a_0 // 0
-    p += a(0)*a(1) + 1 // 1
 
-    val q: ArrayBuffer[BigInt] = ArrayBuffer()
-    q += 1 // 0
-    q += a(1) // 1
+    var p_n: BigInt = a(0) * a(1) + 1
+    var p_n1: BigInt = a_0
+    var p_n2: BigInt = 0
 
+    var q_n: BigInt = a(1)
+    var q_n1: BigInt = 1
+    var q_n2: BigInt = 0
 
     var n = 1
 
-    while(a(n) != a_0*2){
-      //while(!test(p(n).toBigInt(), q(n).toBigInt, BigInt(D.toLong))){
+    while (a(n) != a_0 * 2) {
       n += 1
-      P += a(n-1)*Q(n-1) - P(n-1) // (n)
-      Q += (D - P(n)*P(n))/Q(n-1) // (n)
-      a += ((a_0 + P(n))/Q(n)) // (n)
+      val tmp = (P_n, Q_n)
+      P_n1 = tmp._1
+      Q_n1 = tmp._2
+      P_n = a(n - 1) * Q_n1 - P_n1
+      Q_n = (D - P_n * P_n) / Q_n1 // (n)
+      a += ((a_0 + P_n) / Q_n) // (n)
 
-      p += a(n)*p(n-1) + p(n-2) // (n)
-      q += a(n)*q(n-1) + q(n-2) // (n)
+      p_n2 = p_n1
+      p_n1 = p_n
+      p_n = a(n) * p_n1 + p_n2
+
+      q_n2 = q_n1
+      q_n1 = q_n
+      q_n = a(n) * q_n1 + q_n2
+
     }
-    //(p(n).toBigInt(), q(n).toBigInt())
 
-    val r = n-1
+    val r = n - 1
 
-    require(a(r+1) == a(0)*2)
-    if(r%2 == 1){
-      (p(r), q(r))
-    }else{
+    require(a(r + 1) == a(0) * 2)
+    if (r % 2 == 1) {
+      (p_n1, q_n1)
+    } else {
       var i = n
       val a_sub = a.tail
 
-      while(i < 2*r + 1){
+      while (i < 2 * r + 1) {
         i += 1
-        //P += a(i-1)*Q(i-1) - P(i-1) // (n)
-        //Q += (D - P(i)*P(i))/Q(i-1) // (n)
-        a += a(i % a_sub.size)//math.floor((a_0 + P(i))/Q(i)) // (n)
 
-        p += a(i)*p(i-1) + p(i-2) // (n)
-        q += a(i)*q(i-1) + q(i-2) // (n)
+        val a_i = a(i % a_sub.size)
+
+        p_n2 = p_n1
+        p_n1 = p_n
+        p_n = a_i * p_n1 + p_n2
+
+        q_n2 = q_n1
+        q_n1 = q_n
+        q_n = a_i * q_n1 + q_n2
       }
-      //val size = a.length - 1
 
-      //val r = 1+((2*n+1)% size)
-      (p(2*r+1), q(2*r+1))
+      (p_n, q_n)
     }
 
   }
